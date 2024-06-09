72621c648631ddf138397ed579487d80f7ec80ec
Neddy <Ned@gmail.com>
2024-06-09 17:14:26 +0300

    Добавьте файлы проекта.

---
+++ Chat/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string NicknameFrom { get; set; }
+        public string NicknameTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+        public static Message? DeserializeFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+        public void Print()
+        {
+            Console.WriteLine($"{this.DateTime} Получено сообщение: {this.Text} от {this.NicknameFrom}");
+        }
+    }
+}
---
+++ Chat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server("Server");
+        }
+        public static void Server(string name)
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Сервер ждет сообщение от клиента");
+
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                Message message = Message.DeserializeFromJson(messageText);
+                message.Print();
+
+                byte[] reply = Encoding.UTF8.GetBytes("Сообщение получино");
+                udpClient.Send(reply, reply.Length, iPEndPoint);
+            }
+        }
+    }
+}
