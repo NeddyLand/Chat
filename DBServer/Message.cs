e9be2e8d28a24e616e026926e044e380e16f6fd6
Neddy <Ned@gmail.com>
2024-06-27 19:02:45 +0300

    Добавил DBServer

---
+++ DBServer/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBServer
+{
+    internal class Message
+    {
+        public int MessageId { get; set; }
+        public string Text {  get; set; }
+        public DateTime DateSend { get; set; }
+    }
+}
---
+++ DBServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
